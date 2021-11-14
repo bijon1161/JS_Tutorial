fc488f5b36f371a436585119faf03b6c43518e29
bijon <bijonndc@gmail.com>
2021-11-14 12:49:56 +0600

    After Section 12,Newline Break Continue,TimeStamp=3.26.00hrs

---
+++ js/newline.js
@@ -0,0 +1,24 @@
+//break
+let i;
+for (i=0;i<10;i++)
+{
+    if(i==5)
+    {
+        break;
+    }
+    document.write(i);
+    document.write('<br>');
+}
+
+//Continue
+
+let i;
+for (i=0;i<10;i++)
+{
+    if(i==5)
+    {
+        continue;
+    }
+    document.write(i);
+    document.write('<br>');
+}
