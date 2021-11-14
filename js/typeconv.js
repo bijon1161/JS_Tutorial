8a7b0496845ea8ede526f12f047e4951f08d6134
bijon <bijonndc@gmail.com>
2021-11-14 19:33:41 +0600

    After Section 14,Type Conversion,TimeStamp=4.08.00hrs

---
+++ js/typeconv.js
@@ -0,0 +1,13 @@
+let type;
+type="hello";
+console.log(typeof type);
+type= Number(type);
+console.log(type);
+console.log(typeof type);
+
+/*
+Functions
+1. Number()
+2. String()
+3. Boolean()
+*/ 
