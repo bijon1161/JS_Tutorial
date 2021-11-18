535d4eae430f90cf699af13304522a712cc4c1e2
bijon <bijonndc@gmail.com>
2021-11-18 21:10:02 +0600

    After Section 28,This Object,TimeStamp=7.15.15hrs

---
+++ js/this.js
@@ -0,0 +1,14 @@
+let person={
+    firstName: 'Bijon',
+    lastName: 'Mallik',
+    address: 'Tikatuli,Dhaka',
+    sayHello(){
+           console.log("Hello! I am "+this.firstName+" & I've a "+car.brand+" car");
+    }
+};
+let car={
+    brand: 'TATA',
+    model: 'Safari'
+};
+
+person.sayHello();
