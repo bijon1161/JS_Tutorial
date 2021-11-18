eae4eaf48afdfdf82f3f931c69cb7bb642351de4
bijon <bijonndc@gmail.com>
2021-11-18 16:08:03 +0600

    After Section 27,Methods,TimeStamp=7.07.15hrs

---
+++ js/method.js
@@ -0,0 +1,13 @@
+//best way
+let person={
+    firstName: 'Bijon',
+    lastName: 'Mallik',
+    address: 'Tikatuli,Dhaka',
+    sayHello(){
+        console.log("Hello!");
+    }
+};
+
+person.sayHello();
+
+//another 3 ways is written in notes
