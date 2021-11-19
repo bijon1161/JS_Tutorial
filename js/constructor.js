16fe88c132f95b9de223119af51559e295a497f0
bijon <bijonndc@gmail.com>
2021-11-20 02:36:02 +0600

    After Section 34,Object Constructor,TimeStamp=8.29.00hrs

---
+++ js/constructor.js
@@ -0,0 +1,10 @@
+function Student(first,last,age)
+{
+   this.firstName=first;
+   this.lastName=last;
+   this.age=age;
+}
+let student1= new Student("Bijon","Mallik",25);
+let student2= new Student("Eshita","Mallik",25);
+let student3= new Student("Shakil","Mallik",25);
+console.log(student1);
