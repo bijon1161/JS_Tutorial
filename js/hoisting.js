6666e34b96f111c6dc524811fb99d91058d12f8b
bijon <bijonndc@gmail.com>
2021-11-20 21:36:46 +0600

    After Section 37,Hoisting,TimeStamp=8.53.00hrs

---
+++ js/hoisting.js
@@ -0,0 +1,5 @@
+/*
+it will fetch "variable declaration" and "function call" and take their defination at the top before declaration or function call
+THIS AUTOMATED SYSTEM IN JS IS CALLED HOISTING
+//More from net
+*/
